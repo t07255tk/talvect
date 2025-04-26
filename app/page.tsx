f1f2766cab1fa6d41dfe8efda92663d1530f0154
t07255tk <t07255tk@gmail.com>
2025-04-26 19:59:00 +0900

    Add AuthButton and SessionWrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import AuthButton from "@/components/AuthButton";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <AuthButton />
         <Image
           className={styles.logo}
           src="/next.svg"
---
+++ components/AuthButton.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function AuthButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        <h1>こんにちは {session.user?.name} さん！</h1>
+        <button onClick={() => signOut()}>ログアウト</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>ログインしてないよ</h1>
+        <button onClick={() => signIn('google')}>Googleでログイン</button>
+      </div>
+    );
+  }
+}
