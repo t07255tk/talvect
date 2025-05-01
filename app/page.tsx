d6ee44cb17c8ef96c4acc09a9c9fc5dbbe9adbba
Tatsuro <kurawaki@bmc-net.jp>
2025-05-02 07:33:22 +0900

    Added ModeToggle into Login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { LoginForm } from '@/components/login-form'
+import { ModeToggle } from '@/components/ModeToggle'
 import Image from 'next/image'
 
 export default function LoginPage() {
@@ -13,7 +14,10 @@ export default function LoginPage() {
           className='absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale'
         />
       </div>
-      <div className='flex flex-col gap-4 p-6 md:p-10'>
+      <div className='flex flex-col gap-4 p-6 md:p-10 static'>
+        <div className='absolute right-4 top-4'>
+          <ModeToggle />
+        </div>
         <div className='flex flex-1 items-center justify-center'>
           <div className='w-full max-w-xs'>
             <LoginForm />
