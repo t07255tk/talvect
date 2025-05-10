ed146122fbe330564f97fa73fc6f4801a4d8b7f2
t07255tk <t07255tk@gmail.com>
2025-05-10 14:04:48 +0900

    Add getAvailableTags method

---
+++ lib/tags.ts
@@ -0,0 +1,18 @@
+import prisma from '@/prisma/client'
+import { TagDto } from '@/types/tag'
+
+export default async function getAvailableTags(
+  userId: string,
+): Promise<TagDto[]> {
+  return await prisma.tag.findMany({
+    where: {
+      OR: [{ created_by: null }, { created_by: userId }],
+    },
+    select: {
+      id: true,
+      name: true,
+      description: true,
+    },
+    orderBy: { name: 'asc' },
+  })
+}
