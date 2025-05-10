5f17f4c57d7c6df6659ab16bca5dcda77dfe344a
t07255tk <t07255tk@gmail.com>
2025-05-10 14:08:08 +0900

    refactor: change request body from string to string[]

--- app/api/generate-assessment/route.ts
+++ app/api/generate-assessment/route.ts
@@ -1,16 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { generateAssessment } from '@/lib/generateAssessment'
+import { generateAssessment } from '@/lib/assessment'
 import { requireAuth } from '@/lib/requreAuth'
+import getAvailableTags from '@/lib/tags'
 
 export async function POST(req: NextRequest) {
-const { id: userId } = await requireAuth()
+  const { id: userId } = await requireAuth()
+  const { tags } = await req.json()
 
-  const { text } = await req.json()
-  if (!text || typeof text !== 'string') {
-    return NextResponse.json({ error: 'Invalid input' }, { status: 400 })
+  if (!Array.isArray(tags) || tags.some((id) => typeof id !== 'string')) {
+    return NextResponse.json({ error: 'Invalid tag IDs' }, { status: 400 })
   }
 
-  const quiz = await generateAssessment(text)
+  const allTags = await getAvailableTags(userId)
+  const selectedTags = allTags.filter((tag) => tags.includes(tag.id))
 
-  return NextResponse.json({ quiz })
+  if (selectedTags.length === 0) {
+    return NextResponse.json(
+      { error: 'No valid tags selected' },
+      { status: 404 },
+    )
+  }
+
+  const assessment = await generateAssessment(selectedTags)
+
+  return NextResponse.json({ assessment })
 }
