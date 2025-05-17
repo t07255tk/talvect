497292d9a5099e82c96ff8e20ac0008831ffdb6b
t07255tk <t07255tk@gmail.com>
2025-05-17 16:57:21 +0900

    add isValidUUID function

---
+++ lib/validation.ts
@@ -0,0 +1,8 @@
+/**
+ * UUIDv4形式の文字列かを検証するユーティリティ
+ */
+export function isValidUUID(id: string): boolean {
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+    id,
+  )
+}
