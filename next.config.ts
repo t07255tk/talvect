f3c1176a62fdab0b06aeefef9f8eba0538e4ab0c
t07255tk <t07255tk@gmail.com>
2025-05-31 16:17:49 +0900

    enable removeConsole only in production build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
+    removeConsole:
+      process.env.NODE_ENV === 'development'
+        ? false
+        : {
+            exclude: ['error', 'warn', 'info'],
+          },
   },
   /* config options here */
 }
